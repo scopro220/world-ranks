27b30d0253d180d444ab8965b54baa3aa307f755
Scott Provence <scott.provence@yahoo.com>
2021-11-26 17:47:31 -0500

    Filter feature

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 import styles from "@/styles/Home.module.css";
 import SearchInput from "@/components/SearchInput";
 import CountriesTable from "@/components/CountriesTable";
 
 export default function Home({ countries }) {
+  const [keyword, setKeyword] = useState("");
+
+  const filteredCountries = countries.filter(
+    (country) =>
+      country.name.common.toLowerCase().includes(keyword) ||
+      country.region.toLowerCase().includes(keyword) ||
+      country.subregion?.toLowerCase().includes(keyword)
+  );
+
+  const handleInputChange = (e) => {
+    e.preventDefault();
+    setKeyword(e.target.value.toLowerCase());
+  };
+
   return (
     <Layout>
       <div className={styles.counts}>Found {countries.length} countries</div>
 
-      <SearchInput placeholder="Filter by Name, Region or SubRegion" />
+      <SearchInput
+        placeholder="Filter by Name, Region or SubRegion"
+        onChange={handleInputChange}
+      />
 
-      <CountriesTable countries={countries} />
+      <CountriesTable countries={filteredCountries} />
     </Layout>
   );
 }
